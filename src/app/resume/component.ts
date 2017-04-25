ff48ce49442b341b7cf2419776531baa570f8646
Neil Soult <neil.soult@gmail.com>
2017-04-25 00:47:24 -0700

    added resume route to test

---
+++ src/app/resume/component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: './component.html',
+    styleUrls: ['./component.styl']
+})
+export class ResumeComponent {
+
+    title: string = 'Résumé';
+
+}
--- src/app/resume/module.ts
+++ src/app/resume/module.ts
@@ -4,15 +4,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { ResumeModule } from './resume/module';
-
-import { AppComponent } from './app.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { HeaderComponent } from './shared/header/header.component';
+import { routing } from "./routes";
+import { ResumeComponent } from './component';
+import { FooterComponent } from '../shared/footer/footer.component';
+import { HeaderComponent } from '../shared/header/header.component';
 
 @NgModule({
     declarations: [
-        AppComponent,
+        ResumeComponent,
         FooterComponent,
         HeaderComponent
     ],
@@ -20,9 +19,9 @@ import { HeaderComponent } from './shared/header/header.component';
         BrowserModule,
         CommonModule,
         FormsModule,
-        HttpModule
+        HttpModule,
+        routing
     ],
-    providers: [],
-    bootstrap: [AppComponent]
+    providers: []
 })
-export class AppModule { }
+export class ResumeModule { }
