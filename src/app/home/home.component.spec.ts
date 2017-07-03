45f8bcc1ef656259c773eb87ceb59bb21f095a45
Neil Soult <neil.soult@gmail.com>
2017-07-03 12:25:42 -0700

    cleanup, restart

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -3,23 +3,31 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
-  }));
+    let component: HomeComponent;
+    let fixture: ComponentFixture<HomeComponent>;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async(() => {
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        TestBed.configureTestingModule({
+            declarations: [ HomeComponent ]
+        })
+        .compileComponents();
+
+    }));
+
+    beforeEach(() => {
+
+        fixture = TestBed.createComponent(HomeComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+
+    });
+
+    it('should create', () => {
+
+        expect(component).toBeTruthy();
+
+    });
+
 });
